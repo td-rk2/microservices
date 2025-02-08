d7e55743b76e3c4c97961b454a9a654ca709779e
Piyush Karan <piyush.karan@infosys.com>
2025-02-08 18:08:34 +0530

    Exception handling

--- user-service/src/main/java/com/Tz/Exceptions/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/Tz/Exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,19 @@
 package com.Tz.Exceptions;
 
+import com.Tz.payload.response.ExceptionResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> ExceptionHandler(Exception ex, WebRequest req){
+        ExceptionResponse response = new ExceptionResponse(ex.getMessage(), req.getDescription(false), LocalDateTime.now());
+        return ResponseEntity.ok(response);
+    }
 }
--- user-service/src/main/java/com/Tz/payload/response/ExceptionResponse.java
+++ user-service/src/main/java/com/Tz/payload/response/ExceptionResponse.java
@@ -1,4 +1,39 @@
 package com.Tz.payload.response;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExceptionResponse {
+    private String message;
+    private String error;
+    private LocalDateTime timestamp;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
